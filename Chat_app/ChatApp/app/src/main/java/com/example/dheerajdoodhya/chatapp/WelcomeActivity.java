61a4aa573ea5ed9b458776e6bb4389f2cbd0334a
DheerajD <dheeraj.doodhya@gmail.com>
2019-01-16 00:56:22 +0530

    Adding ChatMainActivity

--- Chat_app/ChatApp/app/src/main/java/com/example/dheerajdoodhya/chatapp/WelcomeActivity.java
+++ Chat_app/ChatApp/app/src/main/java/com/example/dheerajdoodhya/chatapp/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.dheerajdoodhya.chatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,21 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent mainIntent = new Intent(WelcomeActivity.this, ChatMainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
     }
 }
